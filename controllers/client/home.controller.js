d0984092b5ff4321243f8fcf17bec973f48dc85c
trungkoderlor <159401260+trungkoderlor@users.noreply.github.com>
2024-09-17 20:48:23 +0700

    update FE, home, header and footer

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -9,8 +9,14 @@ module.exports.index = async (req, res) => {
       item.priceNew= (item.price*(100-item.discountPercentage)/100).toFixed(2);
       return item;
   })
+  const posters = [
+    { src: '/images/poster1.png', alt: 'Poster 1' },
+    { src: '/images/poster2.png', alt: 'Poster 2' },
+    { src: '/images/poster3.png', alt: 'Poster 3' }
+  ];
   res.render('client/pages/home/index',{
       pageTitle: "Trang Chủ",
-      products: newProducts
+      products: newProducts,
+      posters: posters
   });
 }
